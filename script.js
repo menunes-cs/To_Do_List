cfadec451359c7fb22127c66137d4ad30599f7bf
Nunez <dudasaquetonunes16@gmail.com>
2026-07-13 19:43:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function adicionarTarefa(){
+    let valorDoInput = document.querySelector("input").value
+
+    let li = document.createElement("li")
+    li.innerHTML = valorDoInput + '<span onclick="deletarTarefa(this)">❌</span>'
+
+    document.querySelector("ul").appendChild(li)
+
+    document.querySelector('input').value = ''
+
+}
+
+    function deletarTarefa(li){
+        li.parentElement.remove()
+
+}
